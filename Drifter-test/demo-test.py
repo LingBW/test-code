91de331d97cf5c256ddd0426a87493608b561e32
Bingwei Ling <ufwrgw@gmail.com>
2015-01-26 13:30:45 -0500

    Update demo-test.py

--- Drifter-test/demo-test.py
+++ Drifter-test/demo-test.py
@@ -28,10 +28,10 @@ MODE = 'FORCAST'           # 'FORECAST' or 'HINDCAST'
 GRID = 'massbay'            # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay'
 # if raw data, use "drift_X.dat";if want to get drifter data in database, use "None"
 INPUT_DATA = 'drift_X.dat'   
-run_time = datetime(2014,12,20,0,0,0,0,pytz.UTC)
+run_time = datetime(2015,1,24,0,0,0,0,pytz.UTC)
 
 drifter_ids = [140410704,140410706,140410707,140410708,140410709] #[140410701]
-drifter_ID = 140410704
+drifter_ID = 140430701
 ##########################################
 points = {'lats':[],'lons':[]}
 dr_set={'lats':[],'lons':[]}
@@ -58,14 +58,17 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 draw_basemap(fig, ax, points)
 ax.plot(dr_set['lons'][0],dr_set['lats'][0],'c.',markersize=16) #,label='Startpoint'
-ax.annotate('12/20 23:46', xy=(dr_set['lons'][0]+0.005,dr_set['lats'][0]+0.005),fontsize=6,
+ax.annotate('24/1', xy=(dr_set['lons'][0]+0.005,dr_set['lats'][0]+0.005),fontsize=6,
             xytext=(dr_set['lons'][0]+0.03,dr_set['lats'][0]+0.03),
             arrowprops=dict(arrowstyle="wedge")) #facecolor=colors[i]
 #colors=uniquecolors(len(points['lats'])) #config colors
 plt.title('Drifter {0}: {1} track'.format(drifter_ID,MODE))
 ########################
 def animate(n):
-    #ax.cla()   
+    #ax.cla()
+    #del ax.lines() #apply to plot
+    #del ax.collection() #apply to scatter  
+    del ax.lines[:]
     '''del ax.lines[:]
     if n<5:
         ax.plot(fc_set['lons'][n],fc_set['lats'][n],color='r',linestyle='-',linewidth=4,
@@ -77,18 +80,32 @@ def animate(n):
                 marker='o',markerfacecolor='r',markersize=6) #'%d'%n   
         ax.plot(dr_set['lons'][n-5:n],dr_set['lats'][n-5:n],color='g',linestyle='-',linewidth=4,
                 marker='o',markerfacecolor='b',markersize=6,label='drifter')'''
-    ax.plot(fc_set['lons'][n],fc_set['lats'][n],'ro',markersize=4,label='%s'%MODE) 
-    #,markerfacecolor='r''%d'%n ,color='r' ,linewidth=4 ,marker='o'
-    if n<len(dr_set['lons']):
-        po = (dr_set['lons'][n]+0.005,dr_set['lats'][n]+0.005)
-        pt = (dr_set['lons'][n]+0.03,dr_set['lats'][n]+0.03)
-        if n==24:
-            ax.annotate('12/21 23:46',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
-        if n==49:
-            ax.annotate('12/22 23:46',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
-        if n==62:
-            ax.annotate('12/23 11:46',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
-        ax.plot(dr_set['lons'][n],dr_set['lats'][n],'bo',markersize=6,label='Drifter')
+    if n<5:
+        ax.plot(fc_set['lons'][:n],fc_set['lats'][:n],'ro',markersize=(10-n%5),label='%s'%MODE) 
+        #,markerfacecolor='r''%d'%n ,color='r' ,linewidth=4 ,marker='o'
+        if n<len(dr_set['lons']):
+            po = (dr_set['lons'][n]+0.005,dr_set['lats'][n]+0.005)
+            pt = (dr_set['lons'][n]+0.03,dr_set['lats'][n]+0.03)
+            if n==12:
+                ax.annotate('25/1',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
+            if n==23:
+                ax.annotate('26/1',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
+            if n==29:
+                ax.annotate('26/1 12:00',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
+            ax.plot(dr_set['lons'][:n],dr_set['lats'][:n],'bo-',markersize=(10-n%5),label='Drifter')
+    if n>=5:
+        ax.plot(fc_set['lons'][n-5:n],fc_set['lats'][n-5:n],'ro',markersize=(10-n%5),label='%s'%MODE) 
+        #,markerfacecolor='r''%d'%n ,color='r' ,linewidth=4 ,marker='o'
+        if n<len(dr_set['lons']):
+            po = (dr_set['lons'][n]+0.005,dr_set['lats'][n]+0.005)
+            pt = (dr_set['lons'][n]+0.03,dr_set['lats'][n]+0.03)
+            if n==12:
+                ax.annotate('25/1',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
+            if n==23:
+                ax.annotate('26/1',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
+            if n==30:
+                ax.annotate('26/1 14:31',xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
+            ax.plot(dr_set['lons'][n-5:n],dr_set['lats'][n-5:n],'bo-',markersize=(10-n%5),label='Drifter')
     
 anim = animation.FuncAnimation(fig, animate, frames=len(fc_set['lons']), interval=50)
 plt.legend(loc='lower right',fontsize=10)
@@ -96,6 +113,6 @@ en_run_time = datetime.now()
 print 'Take '+str(en_run_time-st_run_time)+' run the code. End at '+str(en_run_time)
 #anim.save('%s multi-days_demo.gif'%drifter_ID, writer='imagemagick',fps=10,dpi=100) #ffmpeg,imagemagick,mencoder fps=20
 anim.save('%s-demo_%s.gif'%(drifter_ID,en_run_time.strftime("%d-DEC-%H:%M")),
-          writer='imagemagick',fps=10,dpi=100) #ffmpeg,imagemagick,mencoder fps=20
+          writer='imagemagick',fps=5,dpi=150) #ffmpeg,imagemagick,mencoder fps=20
 plt.show()
 
