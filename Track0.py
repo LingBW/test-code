55b4c0279ab5b9f8746e237557244529d5c9354d
Bingwei Ling <ufwrgw@gmail.com>
2015-01-30 12:58:41 -0500

    Update Track0.py

--- Track0.py
+++ Track0.py
@@ -9,75 +9,118 @@ the drifter track for the next two days.
 """
 
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import pytz
+from makegrid import clickmap, points_between
 from track_functions import get_drifter,get_fvcom,get_roms,draw_basemap,distance,uniquecolors
 from matplotlib import animation
 st_run_time = datetime.now() # Caculate the time running the code with en_run_time
 
-############################### Options #######################################
-'''
-Option 1: 
-Option 2:
-Option 3:
-'''
-Option = 1  # 2,3
-#################
-if Option == 1:    
-    drifter_ID = 140420691 #[140410704,140410706,140410707,140410708,140410709] 140430701]
-    # if raw data, use "drift_X.dat";if want to get drifter data in database, use "None"
-    INPUT_DATA = 'drift_X.dat' 
-    MODEL = 'Drifter'              # 'FVCOM', 'ROMS' or 'BOTH'                  
-    GRID = 'massbay'            # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay' (needed foe FVCOM model only)
-    MODE = 'FORECAST'           # 'FORECAST' or 'HINDCAST'
-    DEPTH = -1.               # depth of drogue in meters
-    DAYS = 3                     # Length of time wanted in track
-    run_time = datetime(2015,1,24,0,0,0,0,pytz.UTC)
-    print "Drifter: %s track %d days"%(drifter_ID,DAYS)
-elif Option==2: # user specified pts
-    MODEL = 'FVCOM'
-    track_way = 'forwards' # two options: forwards,backwards
-    GRID = 'massbaya'     # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay'
-    point1 = (41.729686, -70.636645)
-    point2 = (41.686903, -70.665452)
-    (lat,lon)=points_between(point1,point2,1)   
-    print 'You added %d points.' % len(lat)
-elif Option == 3: # click on a map
-    MEDOL ="FVCOM"
-    track_way = 'forwards' # two options: forwards,backwards
-    GRID = 'massbaya'     # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay'
-    numpts=9 # at most added on the map
-    [lon,lat]=clickmap(numpts) # gets lat/lon by clicking map
-    print 'You added %d points.' % len(lat)
-############################## Extract Data ###################################
-if MODEL=='Drifter':
-    drifter = get_drifter(drifter_ID, INPUT_DATA)
-    dr_points = drifter.get_track(begin_time,1)
-    dr_set['lons'].extend(dr_points['lon']); dr_set['lats'].extend(dr_points['lat'])
-if MODEL=='FVCOM':
+def extract_fvcom_point(sart_time, end_time, st_lon, st_lat):       
+    DEPTH = -1
+    GRID = 'massbay'     # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay'
     get_obj = get_fvcom(GRID)
     url_fvcom = get_obj.get_url(start_time,end_time)
-    point = get_obj.get_track(st_point[0],st_point[1],DEPTH,url_fvcom)
-if MODEL=='Drifter':
+    point = get_obj.get_track(st_lon,st_lat,DEPTH,url_fvcom)
+    return point
+def extract_roms_point(sart_time, end_time, st_lon, st_lat):
+    DEPTH = -1
     get_obj = get_roms()
-    url_roms = get_obj.get_url(start_time, end_time)
-    point = get_obj.get_track(st_point[0],st_point[1],DEPTH,url_roms) #includes start point
+    url_roms = get_obj.get_url(start_time,end_time)
+    point = get_obj.get_track(st_lon,st_lat,DEPTH,url_roms)
+    return point
+############################### Options #######################################
+'''
+Option 1: ...
+Option 2: ....
+Option 3: ....
+'''
+Option = 3  # 2,3
+#################
+if Option == 1:    
+    drifter_ID = 140420691 #[140410704,140410706,140410707,140410708,140410709] 140430701] 
+    start_time = datetime(2015,1,24,0,0,0,0,pytz.UTC)           
+    forecast_days = 2
+    MODEL = 'FVCOM'              # 'FVCOM', 'ROMS' or 'BOTH'                     
+    #MODE = 'FORECAST'           # 'FORECAST' or 'HINDCAST'
+    INPUT_DATA = 'drift_X.dat'# if raw data, use "drift_X.dat";if want to get drifter data in database, use "None"    
+    print "Drifter: %s forecast %d days"%(drifter_ID,forecast_days)
+elif Option==2: # user specified pts    
+    start_time = datetime(2015,1,28,0,0,0,0,pytz.UTC)
+    forecast_days = 1
+    MODEL = 'FVCOM'
+    #track_way = 'forwards' # two options: forwards,backwards   
+    point1 = (42.013508, -70.289329)  # Point data structure:(lat,lon)
+    point2 = ()#41.686903, -70.665452#41.876636, -70.410178
+    (st_lat,st_lon)=points_between(point1,point2,1)
+    print st_lat,st_lon
+    stp_num = len(st_lat)
+    print 'You added %d points.' % stp_num
+elif Option == 3: # click on a map    
+    start_time = datetime(2015,1,28,0,0,0,0,pytz.UTC)
+    forecast_days = 1
+    MODEL = 'FVCOM'
+    #track_way = 'forwards' # two options: forwards,backwards
+    numpts=9 # at most added on the map
+    [st_lon,st_lat]=clickmap(numpts) # gets lat/lon by clicking map
+    print st_lat,st_lon
+    stp_num = len(st_lat)
+    print 'You added %d points.' % stp_num
+############################## Extract Data ###################################
+drifter_points = dict(lon=[],lat=[])
+model_points = dict(lon=[],lat=[])
+#fvcom_points = dict(lon=[],lat=[])
+if Option == 1:
+    drifter = get_drifter(drifter_ID, INPUT_DATA)
+    dr_points = drifter.get_track(start_time,)
+    drifter_points['lon'].extend(dr_points['lon']); drifter_points['lat'].extend(dr_points['lat'])
+    if MODEL=='FVCOM':
+        int1 = extract_fvcom_point(dr_points['time'][0],dr_points['time'][-1],dr_points['lon'][0],dr_points['lat'][0])
+    if MODEL=='ROMS':
+        int1 = extract_roms_point(dr_points['time'][0],dr_points['time'][-1],dr_points['lon'][0],dr_points['lat'][0])
+    end_time2 = dr_points['time'][-1] + timedelta(forecast_days)
+    if MODEL=='FVCOM':
+        point2 = extract_fvcom_point(dr_points['time'][-1],end_time2,dr_points['lon'][-1],dr_points['lat'][-1])
+    if MODEL=='ROMS':
+        point2 = extract_roms_point(dr_points['time'][-1],end_time2,dr_points['lon'][-1],dr_points['lat'][-1])
+    model_points['lon'].extend(point1['lon']); model_points['lat'].extend(point1['lat'])
+    model_points['lon'].extend(point2['lon']); model_points['lat'].extend(point2['lat'])
+if Option==2 or Option==3:
+    end_time = start_time + timedelta(forecast_days)
+    for i in range(len(st_lat)):
+        if MODEL=='FVCOM': point = extract_fvcom_point(start_time,end_time,st_lon[i],st_lat[i])        
+        if MODEL=='ROMS': point = extract_roms_point(start_time,end_time,st_lon[i],st_lat[i])
+        model_points['lon'].extend(point['lon']); model_points['lat'].extend(point['lat'])
+    #np.save('model_lons.npy',model_points['lon']); np.save('model_lats.npy',model_points['lat'])
+    np.savez('model_points.npz',lon=model_points['lon'],lat=model_points['lat'])
+    try:
+        np.savetxt('model_points.txt',model_points['lon'],delimiter=',')
+    except:
+        print 'It is not working'
+    lon_set = [[]]*stp_num; lat_set = [[]]*stp_num
+    seg_num = len(model_points['lon'])/stp_num
+    for i in xrange(stp_num):
+        lon_set[i] = model_points['lon'][seg_num*i:seg_num*(1+i)]
+        lat_set[i] = model_points['lat'][seg_num*i:seg_num*(1+i)]
+    arr_lon = np.array(lon_set).T; arr_lat = np.array(lat_set).T
+    
+
 ################################## Plot #######################################
 points = {'lats':[],'lons':[]}  # collect all points we've gained
-points['lats'].extend(dr_set['lats']); points['lons'].extend(dr_set['lons'])
-points['lats'].extend(fc_set['lats']); points['lons'].extend(fc_set['lons'])
+points['lats'].extend(model_points['lat']); points['lons'].extend(model_points['lon'])
+points['lats'].extend(drifter_points['lat']); points['lons'].extend(drifter_points['lon'])
 fig = plt.figure() #figsize=(16,9)
 ax = fig.add_subplot(111)
 draw_basemap(fig, ax, points)
-'''ax.plot(dr_set['lons'][0],dr_set['lats'][0],'c.',markersize=16) #,label='Startpoint'
-ax.annotate(an[0], xy=(dr_set['lons'][0]+0.005,dr_set['lats'][0]+0.005),fontsize=6,
-            xytext=(dr_set['lons'][0]+0.03,dr_set['lats'][0]+0.03),
-            arrowprops=dict(arrowstyle="wedge")) #facecolor=colors[i]'''
+#ax.plot(model_points['lon'],model_points['lat'],'ro-',markersize=8) #,label='Startpoint'c.
+plt.title('forecast track')#('Drifter: {0} {1} track'.format(drifter_ID,MODE))
 #colors=uniquecolors(len(points['lats'])) #config colors
-plt.title('Drifter: {0} {1} track'.format(drifter_ID,MODE))
+def animate(n):
+    ax.plot(arr_lon[n],arr_lat[n],'ro',markersize=8,label='forecast')
 ###############
-def animate(n):  # the function of the animation
+'''def animate(n):  # the function of the animation
     #ax.cla()
     #del ax.lines() #apply to plot
     #del ax.collection() #apply to scatter
@@ -111,24 +154,12 @@ def animate(n):  # the function of the animation
             po = (fc_set['lons'][n]+0.005,fc_set['lats'][n]+0.005)
             pt = (fc_set['lons'][n]+0.03,fc_set['lats'][n]+0.03)
             ax.annotate(an3,xy=po,xytext=pt,fontsize=8,arrowprops=dict(arrowstyle="wedge"))
-        ax.plot(fc_set['lons'][a[2]:n],fc_set['lats'][a[2]:n],'ro-',markersize=4,label='%s'%MODE)
-    '''ax.plot(fc_set['lons'][n],fc_set['lats'][n],'ro',markersize=4,label='%s'%MODE) #'%d'%n 
-    if n<len(dr_set['lons']):
-        po = (dr_set['lons'][n]+0.005,dr_set['lats'][n]+0.005)
-        pt = (dr_set['lons'][n]+0.03,dr_set['lats'][n]+0.03)
-        if n==a[0]:
-            ax.annotate(an[1],xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
-        if n==a[1]:
-            ax.annotate(an[2],xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
-        if n==(a[2]-1):
-            ax.annotate(an3,xy=po,xytext=pt,fontsize=6,arrowprops=dict(arrowstyle="wedge"))
-        ax.plot(dr_set['lons'][n],dr_set['lats'][n],'bo-',markersize=6,label='drifter')'''   
-anim = animation.FuncAnimation(fig, animate, frames=len(fc_set['lons']), interval=50)
+        ax.plot(fc_set['lons'][a[2]:n],fc_set['lats'][a[2]:n],'ro-',markersize=4,label='%s'%MODE)'''  
+anim = animation.FuncAnimation(fig, animate, frames=seg_num, interval=50)
 plt.legend(loc='lower right',fontsize=10)
 ###################################################
 en_run_time = datetime.now()
-print 'Take '+str(en_run_time-st_run_time)+' run the code. End at '+str(en_run_time)
-#anim.save('%s multi-days_demo.mp4'%drifter_ID, writer='mencoder',fps=10,dpi=200) 
-anim.save('%s-demo_%s.gif'%(drifter_ID,en_run_time.strftime("%d-DEC-%H:%M")),
-          writer='imagemagick',fps=5,dpi=150) #ffmpeg,imagemagick,mencoder fps=20
+print 'Take '+str(en_run_time-st_run_time)+' running the code. End at '+str(en_run_time) 
+anim.save('%s-demo_%s.gif'%(MODEL,en_run_time.strftime("%d-DEC-%H:%M")),
+          writer='imagemagick',fps=5,dpi=150) #ffmpeg,imagemagick,mencoder fps=20'''
 plt.show()
